aba73fd145a537cc42ba2e58f3a034a0d5972416
felipectr05 <felipectr7@gmail.com>
2023-11-29 15:01:22 -0300

    comentário

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { RouteSoftware } from "./routes";
 import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import Cadastro from './Cadastro';
 
 function App() {
+  return (
+    <div>
+      {/* Outros componentes ou conteúdos podem estar aqui */}
+      <Cadastro />
+    </div>
+  );
+
+  function App() {
 
   return (
     <>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,4 +11,52 @@ export function Home() {
         <ComponentCard text="Dicas para Churrasco Perfeito"/>
     </style.Main>
   )
+  // Cadastro.js
+
+import React, { useState } from 'react';
+
+const Cadastro = () => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Faça algo com os dados (por exemplo, envie para um servidor)
+    console.log('Nome:', nome);
+    console.log('Email:', email);
+    // Limpar o formulário
+    setNome('');
+    setEmail('');
+  };
+
+  return (
+    <div>
+      <h1>Cadastro</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome:
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          E-mail:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Cadastro;
+
 }
